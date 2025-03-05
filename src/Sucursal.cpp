549daf8817e9d45818ab432bd7bff14f91cbbacd
Pablo Miguel Angel Curtti <156843294+PMAC80@users.noreply.github.com>
2025-03-05 19:32:25 -0300

    Update Sucursal.cpp

--- src/Sucursal.cpp
+++ src/Sucursal.cpp
@@ -1,23 +1,40 @@
-#ifndef SUCURSAL_HPP
-#define SUCURSAL_HPP
+#include "Sucursal.hpp"
 
-#include "Empleado.hpp"
 
-class Sucursal 
+Sucursal::Sucursal(string nombre) : nombre(nombre), cantidadEmpleados(0) 
 {
-private:
-    string nombre;
-    Empleado* empleados[10];
-    int cantidadEmpleados;
+    cout << "Sucursal " << nombre << " creada." << endl;
+    cout << "-----------------------------------------" << endl;
+}
 
-public:
-    Sucursal(string nombre);
-    ~Sucursal();
 
-    void agregarEmpleado(Empleado* empleado);
-    void mostrarEmpleados() const;
-    void enviarMensaje(const std::string& mensaje) const;
-    string getNombre() const;
-};
+Sucursal::~Sucursal() {}
 
-#endif
+
+void Sucursal::agregarEmpleado(Empleado* empleado) 
+{
+    if (cantidadEmpleados < 10) 
+    {
+        empleados[cantidadEmpleados++] = empleado; 
+        cout << empleado->getNombre() << " ha sido agregado a la sucursal " << nombre << "." << endl;       
+    } 
+    else 
+    {
+        cout << "No se pueden agregar más empleados a la sucursal " << nombre << "." << endl;
+    }
+}
+
+void Sucursal::mostrarEmpleados() const 
+{
+    cout << "Sucursal: " << nombre << endl;
+    for (int i = 0; i < cantidadEmpleados; ++i) 
+    {
+        empleados[i]->mostrarDatos();
+        empleados[i]->trabajar();
+    }
+}
+
+string Sucursal:: getNombre() const
+{
+    return nombre;
+}
