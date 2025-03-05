2dfd80ff09a23f453e06f5f2d36aec7b64952129
Pablo Miguel Angel Curtti <156843294+PMAC80@users.noreply.github.com>
2025-03-05 19:23:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,60 @@
 #include "VendedorProfesional.hpp"
 #include "VendedorEmpresa.hpp"
 
+void clearConsole();
+void pauseConsole();
+
 int main() {
-    Empresa empresa("Empresa");
 
-    Sucursal* sucursal1 = new Sucursal("Sucursal 1");
-    Sucursal* sucursal2 = new Sucursal("Sucursal 2");
+    clearConsole();
 
-    VendedorParticular* vendedor1 = new VendedorParticular("Juan", 1000);
-    VendedorProfesional* vendedor2 = new VendedorProfesional("Maria", 1500);
-    VendedorEmpresa* vendedor3 = new VendedorEmpresa("Carlos", 2000);
+    Empresa empresa("Mi Empresa");
 
+    Sucursal* sucursal1 = new Sucursal("Mar del Plata");
+    Sucursal* sucursal2 = new Sucursal("Tandil");
+
+    VendedorParticular* vendedor1 = new VendedorParticular("Pablo", 1800);
+    VendedorProfesional* vendedor2 = new VendedorProfesional("Lisandro", 2500);
+    VendedorEmpresa* vendedor3 = new VendedorEmpresa("Franco", 2800);
+
+    cout << "-----------------------------------------"<< endl;
+    empresa.agregarSucursal(sucursal1);
+    empresa.agregarSucursal(sucursal2);
+ 
+    cout << "-----------------------------------------"<< endl;
     sucursal1->agregarEmpleado(vendedor1);
     sucursal1->agregarEmpleado(vendedor2);
     sucursal2->agregarEmpleado(vendedor3);
-
-    empresa.agregarSucursal(sucursal1);
-    empresa.agregarSucursal(sucursal2);
-
+    
+    cout << "-----------------------------------------"<< endl;
     empresa.mostrarSucursales();
-
-    empresa.enviarMensaje(vendedor1, "Recordatorio 1");
-    empresa.enviarMensaje(vendedor2, "Recordatorio 2");
-    empresa.enviarMensaje(vendedor3, "Recordatorio 3");
-
+    
+    cout << "-----------------------------------------"<< endl;
+    empresa.enviarMensaje(vendedor1, "Estudia para la materia de Jose");
+    empresa.enviarMensaje(vendedor2, "Lava los platos");
+    empresa.enviarMensaje(vendedor3, "No dejes todo tirado");
+    
+    cout << "-----------------------------------------"<< endl;
+    delete vendedor1;
+    delete vendedor2;
+    delete vendedor3;
+    
     return 0;
 }
+
+void clearConsole() 
+{
+    #ifdef __unix__
+        system("clear");
+    #elif __APPLE__
+        system("clear");
+    #elif defined(_WIN32) || defined(WIN64)
+        system("cls");
+    #endif
+}
+void pauseConsole()                                        
+{
+cout << "Presione Enter para continuar..." << endl;
+cin.ignore();                                               
+cin.get();                                                  
+}  
