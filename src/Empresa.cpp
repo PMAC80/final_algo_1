34f8bc65e6b64cb32a878cf0fa5f46f005979920
Pablo Miguel Angel Curtti <156843294+PMAC80@users.noreply.github.com>
2025-03-05 19:25:19 -0300

    Update Empresa.cpp

--- src/Empresa.cpp
+++ src/Empresa.cpp
@@ -1,41 +1,27 @@
+#ifndef EMPRESA_HPP
+#define EMPRESA_HPP
+
 #include <iostream>
-#include "Empresa.hpp"
+#include <string>
+#include "Sucursal.hpp"
 
 using namespace std;
 
-Empresa::Empresa(string nombre) : nombre(nombre), cantidadSucursales(0) 
+class Empresa 
 {
-    cout << "Empresa " << nombre << " creada." << endl;
-}
+private:
+    string nombre;
+    Sucursal* sucursales[10];
+    int cantidadSucursales;
 
-Empresa::~Empresa() 
-{
-    for (int i = 0; i < cantidadSucursales; i++)
-    {
-        delete sucursales[i];
-        cout << "Sucursal " << i << " destruida." << endl;
-    }
+public:
+    Empresa(string nombre);
+    ~Empresa();
+
+    void agregarSucursal(Sucursal* sucursal);
+    void mostrarSucursales() const;
+    void enviarMensaje(Empleado* empleado, const std::string& mensaje) const;
     
-}
+};
 
-
-void Empresa::agregarSucursal(Sucursal* sucursal) {
-    if (cantidadSucursales < 10) {
-        sucursales[cantidadSucursales++] = sucursal;
-    } else {
-        cout << "No se pueden agregar más sucursales a la empresa " << nombre << "." << endl;
-    }
-}
-
-void Empresa::mostrarSucursales() const {
-    cout << "Empresa: " << nombre << endl;
-    for (int i = 0; i < cantidadSucursales; ++i) {
-        sucursales[i]->mostrarEmpleados();
-    }
-}
-
-void Empresa::enviarMensaje(Empleado* empleado, const std::string& mensaje) const {
-    if (empleado != nullptr) {
-        empleado->recibirMensaje(mensaje);  
-    }
-}
+#endif
